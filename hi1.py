2c27dd582fd76ffcb23c45280b4c6649033447e3
ShaliniN19 <shalininaveenn.19@gmail.com>
2025-03-05 16:12:50 +0530

    hi

--- hi1.py
+++ hi1.py
@@ -3,10 +3,10 @@ content = """
 <!DOCTYPE html>
 <html>
 <head>
-<title>webserver</title>
+<title>My webserver</title>
 </head>
 <body>
-<h1>Welcome to my web page<h1>
+<h1>Welcome<h1>
 </body>
 </html>
 """
@@ -17,7 +17,7 @@ class myhandler(BaseHTTPRequestHandler):
         self.send_header('content-type', 'text/html; charset=utf-8')
         self.end_headers()
         self.wfile.write(content.encode())
-server_address = ('',80)
+server_address = ('',8000)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
